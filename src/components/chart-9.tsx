10ca877f57841764d6903985fdc7f770510aa9b4
Yinjtcflm <1529792769@163.com>
2023-01-27 22:55:49 +0800

    完成chart9

--- src/components/chart-9.tsx
+++ src/components/chart-9.tsx
@@ -1,13 +1,64 @@
 import React, { useEffect, useRef } from "react";
 import * as echarts from "echarts";
+import { px } from "../shared/px";
+import { createEchartsOptions } from "../shared/create-echarts-options";
 
 export const Chart9 = () => {
   const divRef = useRef(null);
   useEffect(() => {
     var myChart = echarts.init(divRef.current);
+    myChart.setOption(
+      createEchartsOptions({
+        color: "#f7a110",
+        xAxis: {
+          type: "category",
+          boundaryGap: false,
+          data: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
+          splitLine: { show: true, lineStyle: { color: "#073E78" } },
+          axisTick: { show: false },
+          axisLine: { show: false },
+        },
+        yAxis: {
+          type: "value",
+          splitLine: { lineStyle: { color: "#073e78" } },
+          axisLabel: {
+            formatter(val) {
+              return val * 100 + "%";
+            },
+          },
+        },
+        series: [
+          {
+            name: "故意伤人",
+            type: "line",
+            data: [
+              0.15, 0.13, 0.11, 0.13, 0.14, 0.15, 0.16, 0.18, 0.21, 0.19, 0.17,
+              0.16, 0.15,
+            ],
+            symbol: "circle",
+            symbolSize: px(12),
+            lineStyle: { width: px(2) },
+            areaStyle: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                {
+                  offset: 0,
+                  color: "#d2bf44",
+                },
+                {
+                  offset: 1,
+                  color: "#906925",
+                },
+              ]),
+            },
+          },
+        ],
+      })
+    );
   }, []);
+
   return (
     <div className="年龄段-图3">
+      <h3>犯罪年龄趋势图</h3>
       <div ref={divRef} className="chart"></div>
     </div>
   );
