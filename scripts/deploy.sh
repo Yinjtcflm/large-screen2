02fe7cab19408c70f01c9d4005bb618cdc948d4b
Yinjtcflm <15297927693@163.com>
2023-04-14 16:55:32 +0800

    更新脚本

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m deploy &&
-git remote add origin https://github.com/Yinjtcflm/large-screen.git &&
+git remote add origin https://github.com/Yinjtcflm/large-screen2.git &&
 git push -uf origin master:gh-pages &&
 cd -;
 
