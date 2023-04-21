a53aa9fcba0e6f865d181b4db73ec1be816a18da
Yinjtcflm <15297927693@163.com>
2023-04-21 16:05:16 +0800

    忽略dist,更改base路径

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/large-screen2/dist/",
-  base: "/",
+  base: "./",
   plugins: [reactRefresh()],
 });
